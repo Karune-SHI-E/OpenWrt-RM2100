103e9d907b156b6d131310955896efa0b86bed31
Karune-SHI-E <miku5201314520@gmail.com>
2022-07-01 14:38:56 +0800

    Update posts content

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,6 @@
 
 # Add a feed source #添加订阅源
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-echo src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default #自动同步更新上游库软件
-echo src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default #国内常用OpenWrt软件包源码合集，每天自动更新，建议使用lean源码
- echo src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default #国内常用OpenWrt软件包源码合集，每天两次自动更新上游源码。
+sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default #自动同步更新上游库软件
+sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default #国内常用OpenWrt软件包源码合集，每天自动更新，建议使用lean源码
+ sed -i '$a src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default #国内常用OpenWrt软件包源码合集，每天两次自动更新上游源码。
