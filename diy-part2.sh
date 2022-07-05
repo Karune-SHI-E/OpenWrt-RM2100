ff5b8527123c4b65e0ce79175e9212cb2d78f8d2
Karune-SHI-E <miku5201314520@gmail.com>
2022-07-05 14:26:43 +0800

    Update posts content

--- diy-part2.sh
+++ diy-part2.sh
@@ -55,13 +55,13 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' packag
 
 # 修改默认wifi名称ssid为Karune/Karune_5G
 #5g
-sed -i '/ set wireless.default_radio0${devidx}.ssid=OpenWrt/a\set wireless.default_radio0${devidx}.ssid=Karune_5G' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/ set wireless.default_radio0${devidx}.ssid=OpenWrt/a\set wireless.default_radio0${devidx}.ssid=Karune_5G' package/kernel/mt7621/files/lib/wifi/mt7621.sh
 #2.4g
-sed -i '/ set wireless.default_radio1${devidx}.ssid=OpenWrt/a\set wireless.default_radio1${devidx}.ssid=Karune' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/ set wireless.default_radio1${devidx}.ssid=OpenWrt/a\set wireless.default_radio1${devidx}.ssid=Karune' package/kernel/mt7621/files/lib/wifi/mt7621.sh
 
 #wifi加密方式encryption=sae-mixed
 #使用sed 在第四行后添加新字(wifi加密方式，没有是none,wifi密码key为miku5201314521)
 #2.4g
-sed -i '/set wireless.default_radio0${devidx}.encryption=sae-mixed/a\set wireless.default_radio0${devidx}.key=gds.miku5201314521' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/set wireless.default_radio0${devidx}.encryption=sae-mixed/a\set wireless.default_radio0${devidx}.key=gds.miku5201314521' package/kernel/mt7621/files/lib/wifi/mt7621.sh
 #5g
-sed -i '/set wireless.default_radio1${devidx}.encryption=sae-mixed/a\set wireless.default_radio1${devidx}.key=miku5201314521' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/set wireless.default_radio1${devidx}.encryption=sae-mixed/a\set wireless.default_radio1${devidx}.key=miku5201314521' package/kernel/mt7621/files/lib/wifi/mt7621.sh
