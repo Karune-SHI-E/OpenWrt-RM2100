7e6c51f36b5ecafe85f852f620f7d0c792d9c914
Karune-SHI-E <miku5201314520@gmail.com>
2022-07-09 00:46:06 +0800

    Update posts content

--- diy-part3.sh
+++ diy-part3.sh
@@ -9,9 +9,17 @@
 # File name: diy-part3.sh
 # Description: OpenWrt DIY script part 3 (Before Update feeds)
 #============================================================
-#mkdi openwrt/package/lean/luci-theme-argonne/htdocs/luci-static/argonne/background
-
-#----------------------------------------------------------------------------
+#bg1为1920*1080
+#bg2为2000*1247
+#bg3为2000*1168
+#logo为512*512
+#main_bg为200081168
+#main_bg_5k为2500*2109
+#favicon为128*128
+#------------------------------------------------------------------------------------------------------------------------
 mkdir openwrt/package/lean/luci-theme-argonne/htdocs/luci-static/argonne/background
-#----------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------
 cp diy/haide.mp4 openwrt/package/lean/luci-theme-argonne/htdocs/luci-static/argonne/background/haide.mp4
+cp diy/banner openwrt/package/base-files/files/etc/banner
+#------------------------------------------------------------------------------------------------------------------------
+#
